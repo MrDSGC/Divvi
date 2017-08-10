5a47650048158e7d853b3135c4c707fd71ea5159
MrDSGC <dcheng47@gmail.com>
2017-08-10 14:34:46 -0700

    finished belong relation

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,2 +1,12 @@
 class Membership < ApplicationRecord
+  validates :member_id, :group_id, presence: true
+
+  belongs_to :user,
+  foreign_key: :member_id,
+  class_name: "User"
+
+  belongs_to :group,
+  foreign_key: :group_id,
+  class_name: "Group"
+
 end
